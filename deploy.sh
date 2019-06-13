0a721ad42672aa12259a7ae9edd0db727992713a
andrei <andrei@100danish.com>
2019-06-13 15:31:23 +0300

    Update travis

--- deploy.sh
+++ deploy.sh
@@ -2,46 +2,25 @@
 
 PLUGIN_SLUG="${PWD##*/}"
 
-echo "https://plugins.svn.wordpress.org/$PLUGIN_SLUG"
-
-echo "Change plugin version to $TRAVIS_TAG"
 sed -i -e "s/__STABLE_TAG__/$TRAVIS_TAG/g" ./src/readme.txt
 sed -i -e "s/__STABLE_TAG__/$TRAVIS_TAG/g" ./src/${PLUGIN_SLUG}.php
 
-echo 111111111111111111
 cat ./src/readme.txt
 
 # 1. Clone complete SVN repository to separate directory
-echo "Checkout https://plugins.svn.wordpress.org/$PLUGIN_SLUG to ./svn"
 svn co "https://plugins.svn.wordpress.org/$PLUGIN_SLUG" ./svn  --depth immediates
 
-echo "Files:"
-ls ./svn
-
 # 2. Copy git repository contents to SNV trunk/ directory
-echo "Copy from ./src/ to ./svn/trunk/"
 cp -R ./src/* ./svn/trunk
-
-echo "Copy from ./wp_org/assets/ to ./svn/assets/"
 cp -R ./wp_org/assets/* ./svn/assets
 
-echo 222222222222222222
-cat ./svn/trunk/readme.txt
-
 # 3. Switch to SVN repository
-echo "Switch to ./svn"
 cd ./svn
 
-# 7. Create SVN tag
-echo "Copy from trunk to tags/$TRAVIS_TAG"
-svn cp trunk tags/$TRAVIS_TAG
+svn add trunk/*
+svn ci  --message "Release $TRAVIS_TAG" \
+        --username ${SVN_USERNAME} \
+        --password ${SVN_PASSWORD} \
+        --non-interactive
 
-echo 33333333333333333
-cat ./svn/trunk/tags/readme.txt
-
-# 8. Push SVN tag
-#echo "Commit $TRAVIS_TAG"
-#svn ci  --message "Release $TRAVIS_TAG" \
-#        --username $SVN_USERNAME \
-#        --password $SVN_PASSWORD \
-#        --non-interactive
+svn copy https://plugins.svn.wordpress.org/${PLUGIN_SLUG}/trunk https://plugins.svn.wordpress.org/${PLUGIN_SLUG}/tags/${TRAVIS_TAG} -m "Release ${TRAVIS_TAG}"
