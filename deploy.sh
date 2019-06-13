dd56b85837dac1ff5580b2c944867c7a72e12166
andrei <andrei@100danish.com>
2019-06-13 14:13:55 +0300

    Update autoloader

--- deploy.sh
+++ deploy.sh
@@ -8,29 +8,29 @@ echo "Change plugin version to $TRAVIS_TAG"
 sed -i -e "s/__STABLE_TAG__/$TRAVIS_TAG/g" ./src/readme.txt
 sed -i -e "s/__STABLE_TAG__/$TRAVIS_TAG/g" ./src/${PLUGIN_SLUG}.php
 
-# Clone complete SVN repository to separate directory
+# 1. Clone complete SVN repository to separate directory
 echo "Checkout https://plugins.svn.wordpress.org/$PLUGIN_SLUG to ./svn"
 svn co "https://plugins.svn.wordpress.org/$PLUGIN_SLUG" ./svn  --depth immediates
 
 echo "Files:"
 ls ./svn
 
-# Copy git repository contents to SNV trunk/ directory
+# 2. Copy git repository contents to SNV trunk/ directory
 echo "Copy from ./src/ to ./svn/trunk/"
 cp -R ./src/* ./svn/trunk/
 
 echo "Copy from ./wp_org/assets/ to ./svn/assets/"
 cp -R ./wp_org/assets/* ./svn/assets/
 
-# Switch to SVN repository
+# 3. Switch to SVN repository
 echo "Switch to ./svn"
 cd ./svn
 
-# Create SVN tag
+# 7. Create SVN tag
 echo "Copy from trunk to tags/$TRAVIS_TAG"
-cp -R ./trunk/* ./tags/$TRAVIS_TAG
+svn cp trunk tags/$TRAVIS_TAG
 
-# Push SVN tag
+# 8. Push SVN tag
 echo "Commit $TRAVIS_TAG"
 svn ci  --message "Release $TRAVIS_TAG" \
         --username $SVN_USERNAME \
