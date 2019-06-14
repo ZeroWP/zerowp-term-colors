9abcea4c0ae1b35f255cac41a14dd705d684a101
andrei <andrei@100danish.com>
2019-06-15 00:13:00 +0300

    Update travis

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,11 @@
 
 PLUGIN_SLUG="${PWD##*/}"
 
-#sed -i -e "s/__STABLE_TAG__/$TRAVIS_TAG/g" ./src/readme.txt
-#sed -i -e "s/__STABLE_TAG__/$TRAVIS_TAG/g" ./src/${PLUGIN_SLUG}.php
-
-#cat ./src/readme.txt
-
-# 1. Clone complete SVN repository to separate directory
-#svn co "https://plugins.svn.wordpress.org/$PLUGIN_SLUG" ./svn  --depth immediates
-
-# 2. Copy git repository contents to SNV trunk/ directory
-#cp -R ./src/* ./svn/trunk
-#cp -R ./wp_org/assets/* ./svn/assets
+sed -i -e "s/__STABLE_TAG__/$TRAVIS_TAG/g" ./src/readme.txt
+sed -i -e "s/__STABLE_TAG__/$TRAVIS_TAG/g" ./src/${PWD##*/}.php
+svn co "https://plugins.svn.wordpress.org/$PLUGIN_SLUG" ./svn  --depth immediates
+cp -R ./src/* ./svn/trunk
+cp -R ./wp_org/assets/* ./svn/assets
 
 # 3. Switch to SVN repository
 cd ./svn
