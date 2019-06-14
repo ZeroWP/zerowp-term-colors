b7334292b24bed0d9edfae838ec524278d4bf324
andrei <andrei@100danish.com>
2019-06-15 00:19:29 +0300

    Update travis

--- deploy.sh
+++ deploy.sh
@@ -11,16 +11,25 @@ cp -R ./wp_org/assets/* ./svn/assets
 # 3. Switch to SVN repository
 cd ./svn
 
-svn add --force trunk
-svn add --force assets
+svn add --force trunk  \
+        --message "Release $TRAVIS_TAG" \
+        --username $SVN_USERNAME \
+        --password $SVN_PASSWORD \
+        --non-interactive
 
-svn cp \
-trunk tags/${TRAVIS_TAG} \
---username ${SVN_USERNAME} \
---password ${SVN_PASSWORD}
+svn add --force assets  \
+        --message "Release $TRAVIS_TAG" \
+        --username $SVN_USERNAME \
+        --password $SVN_PASSWORD \
+        --non-interactive
 
-svn ci \
---message "Release $TRAVIS_TAG" \
---username ${SVN_USERNAME} \s
---password ${SVN_PASSWORD} \
---non-interactive
+svn cp trunk tags/$TRAVIS_TAG  \
+        --message "Release $TRAVIS_TAG" \
+        --username $SVN_USERNAME \
+        --password $SVN_PASSWORD \
+        --non-interactive
+
+svn ci  --message "Release $TRAVIS_TAG" \
+        --username $SVN_USERNAME \
+        --password $SVN_PASSWORD \
+        --non-interactive
