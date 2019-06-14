effd76e38b9fe37c88469a0b50d79f171d1e1335
andrei <andrei@100danish.com>
2019-06-15 00:06:42 +0300

    Update travis

--- deploy.sh
+++ deploy.sh
@@ -2,25 +2,31 @@
 
 PLUGIN_SLUG="${PWD##*/}"
 
-sed -i -e "s/__STABLE_TAG__/$TRAVIS_TAG/g" ./src/readme.txt
-sed -i -e "s/__STABLE_TAG__/$TRAVIS_TAG/g" ./src/${PLUGIN_SLUG}.php
+#sed -i -e "s/__STABLE_TAG__/$TRAVIS_TAG/g" ./src/readme.txt
+#sed -i -e "s/__STABLE_TAG__/$TRAVIS_TAG/g" ./src/${PLUGIN_SLUG}.php
 
-cat ./src/readme.txt
+#cat ./src/readme.txt
 
 # 1. Clone complete SVN repository to separate directory
-svn co "https://plugins.svn.wordpress.org/$PLUGIN_SLUG" ./svn  --depth immediates
+#svn co "https://plugins.svn.wordpress.org/$PLUGIN_SLUG" ./svn  --depth immediates
 
 # 2. Copy git repository contents to SNV trunk/ directory
-cp -R ./src/* ./svn/trunk
-cp -R ./wp_org/assets/* ./svn/assets
+#cp -R ./src/* ./svn/trunk
+#cp -R ./wp_org/assets/* ./svn/assets
 
 # 3. Switch to SVN repository
 cd ./svn
 
-svn add trunk/*
-svn ci  --message "Release $TRAVIS_TAG" \
-        --username ${SVN_USERNAME} \
-        --password ${SVN_PASSWORD} \
-        --non-interactive
+svn add --force trunk
+svn add --force assets
 
-svn copy https://plugins.svn.wordpress.org/${PLUGIN_SLUG}/trunk https://plugins.svn.wordpress.org/${PLUGIN_SLUG}/tags/${TRAVIS_TAG} -m "Release ${TRAVIS_TAG}"
+svn cp \
+trunk tags/${TRAVIS_TAG} \
+--username ${SVN_USERNAME} \
+--password ${SVN_PASSWORD}
+
+svn ci \
+--message "Release $TRAVIS_TAG" \
+--username ${SVN_USERNAME} \s
+--password ${SVN_PASSWORD} \
+--non-interactive
