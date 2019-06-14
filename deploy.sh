20151bdd127b85481f2f90da9ededb6e752a11a1
andrei <andrei@100danish.com>
2019-06-15 00:31:30 +0300

    Update travis

--- deploy.sh
+++ deploy.sh
@@ -4,24 +4,15 @@ PLUGIN_SLUG="${PWD##*/}"
 
 sed -i -e "s/__STABLE_TAG__/$TRAVIS_TAG/g" ./src/readme.txt
 sed -i -e "s/__STABLE_TAG__/$TRAVIS_TAG/g" ./src/${PWD##*/}.php
-svn co "https://plugins.svn.wordpress.org/$PLUGIN_SLUG" ./svn  --depth immediates
+svn co "https://plugins.svn.wordpress.org/$PLUGIN_SLUG" ./svn
 cp -R ./src/* ./svn/trunk
 cp -R ./wp_org/assets/* ./svn/assets
 
 # 3. Switch to SVN repository
 cd ./svn
 
-svn add --force trunk  \
-        --message "Release $TRAVIS_TAG" \
-        --username $SVN_USERNAME \
-        --password $SVN_PASSWORD \
-        --non-interactive
-
-svn add --force assets  \
-        --message "Release $TRAVIS_TAG" \
-        --username $SVN_USERNAME \
-        --password $SVN_PASSWORD \
-        --non-interactive
+svn add --force trunk
+svn add --force assets
 
 svn cp trunk tags/$TRAVIS_TAG  \
         --message "Release $TRAVIS_TAG" \
